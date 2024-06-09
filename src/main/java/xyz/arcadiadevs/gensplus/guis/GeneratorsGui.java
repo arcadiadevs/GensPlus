d8de271ea49f8c1a61c3831617978a119d6c7cf4
Marko675 <maki.marko.09@gmail.com>
2024-06-09 22:52:10 +0200

    Added the option to change buttons in the GeneratorsGui, including next-page, previous-page, and close-button.

--- src/main/java/xyz/arcadiadevs/gensplus/guis/GeneratorsGui.java
+++ src/main/java/xyz/arcadiadevs/gensplus/guis/GeneratorsGui.java
@@ -3,21 +3,23 @@ package xyz.arcadiadevs.gensplus.guis;
 import com.awaitquality.api.spigot.chat.ChatUtil;
 import com.cryptomorin.xseries.XMaterial;
 import com.cryptomorin.xseries.XSound;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
 import net.milkbowl.vault.economy.Economy;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import xyz.arcadiadevs.gensplus.GensPlus;
+import xyz.arcadiadevs.gensplus.models.GeneratorsData;
+import xyz.arcadiadevs.gensplus.utils.GuiUtil;
 import xyz.arcadiadevs.gensplus.utils.config.Config;
+import xyz.arcadiadevs.gensplus.utils.config.message.Messages;
 import xyz.arcadiadevs.guilib.Gui;
 import xyz.arcadiadevs.guilib.GuiItem;
 import xyz.arcadiadevs.guilib.GuiItemType;
 import xyz.arcadiadevs.guilib.ItemBuilder;
-import xyz.arcadiadevs.gensplus.models.GeneratorsData;
-import xyz.arcadiadevs.gensplus.utils.config.message.Messages;
-import xyz.arcadiadevs.gensplus.utils.GuiUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 /**
  * The GeneratorsGui class provides functionality for opening the generators GUI in GensPlus.
@@ -81,21 +83,34 @@ public class GeneratorsGui {
           .lore(lore)
           .build();
 
-      if (config.getBoolean(Config.GUIS_GENERATORS_GUI_BORDER_ENABLED.getPath())) {
-        GuiUtil.addBorder(menu,
-            config.getString(Config.GUIS_GENERATORS_GUI_BORDER_MATERIAL.getPath())
-        );
+      if ((Config.GUIS_GENERATORS_GUI_BORDER_ENABLED.getBoolean())) {
+        GuiUtil.addBorder(menu, Config.GUIS_GENERATORS_GUI_BORDER_MATERIAL.getString());
       }
 
-      ItemStack nextPage = new ItemBuilder(XMaterial.ARROW.parseItem())
+      Material nextPageMaterial = XMaterial.matchXMaterial(
+          Config.GUIS_GENERATORS_GUI_NEXT_PAGE_MATERIAL.getString())
+          .orElse(XMaterial.ARROW)
+          .parseMaterial();
+
+      Material previousPageMaterial = XMaterial.matchXMaterial(
+          Config.GUIS_GENERATORS_GUI_PREVIOUS_PAGE_MATERIAL.getString())
+          .orElse(XMaterial.ARROW)
+          .parseMaterial();
+
+      Material closeButtonMaterial = XMaterial.matchXMaterial(
+          Config.GUIS_GENERATORS_GUI_CLOSE_BUTTON_MATERIAL.getString())
+          .orElse(XMaterial.BARRIER)
+          .parseMaterial();
+
+      ItemStack nextPage = new ItemBuilder(nextPageMaterial)
           .name(ChatUtil.translate("&aNext Page"))
           .build();
 
-      ItemStack previousPage = new ItemBuilder(XMaterial.ARROW.parseItem())
+      ItemStack previousPage = new ItemBuilder(previousPageMaterial)
           .name(ChatUtil.translate("&aPrevious Page"))
           .build();
 
-      ItemStack closeButton = new ItemBuilder(XMaterial.BARRIER.parseItem())
+      ItemStack closeButton = new ItemBuilder(closeButtonMaterial)
           .name(ChatUtil.translate("&cClose"))
           .build();
 
