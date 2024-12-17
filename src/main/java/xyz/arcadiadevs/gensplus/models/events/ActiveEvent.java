8e29adba32a6b7f020bc021c904c2070d3ddc524
Marko675 <maki.marko.09@gmail.com>
2024-12-17 18:16:06 +0100

    Replace rework branch with local project files

--- src/main/java/xyz/arcadiadevs/gensplus/models/events/ActiveEvent.java
+++
@@ -1,8 +0,0 @@
-package xyz.arcadiadevs.gensplus.models.events;
-
-/**
- * The ActiveEvent record represents an active event in GensPlus.
- * It stores information about the event, its start time, and end time.
- */
-public record ActiveEvent(Event event, long startTime, long endTime) {
-}
